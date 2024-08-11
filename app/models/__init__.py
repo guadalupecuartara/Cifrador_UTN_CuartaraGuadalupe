aa42bd5d0781894be3db679a9f566183c6f930a0
guadalupecuartara <guadaluc08@gmail.com>
2024-08-11 02:52:35 -0300

    perfil agregado

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from .user import User
 from .role import Role
 from .user_data import UserData
+from .profile import Profile
 from .text import Text
 from .text_history import TextHistory
 from .encrypted_content import EncryptedContent
